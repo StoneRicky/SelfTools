5fbb0f126b84c8223ee4e8d172580a3d8d3e0c0d
StoneRicky <739799980@qq.com>
2024-08-12 11:26:10 +0800

    修改距离单位

--- LocationDistance.py
+++ LocationDistance.py
@@ -8,7 +8,7 @@ def calculate_distance(coord1, coord2):
     :param coord2: 第二个坐标，格式为 (纬度, 经度)
     :return: 距离（单位：千米）
     """
-    distance = geodesic(coord1, coord2).kilometers
+    distance = geodesic(coord1, coord2).m
     return distance
 
 # 示例坐标
@@ -16,7 +16,7 @@ coordinate1 = (36.070467, 120.313896)
 coordinate2 = (36.070657, 120.313892)  
 
 # 计算距离
-distance = calculate_distance(coordinate1, coordinate2) * 1000
+distance = calculate_distance(coordinate1, coordinate2)
 print('经纬度距离计算')
 print('A坐标为：',coordinate1)
 print('B坐标为：',coordinate2)
