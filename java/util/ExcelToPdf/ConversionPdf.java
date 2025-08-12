c8e276ed3ff66227ae53391b3b3e2b92cf15a478
StoneRicky <739799980@qq.com>
2025-08-12 16:44:07 +0800

    提交相关工具代码

---
+++ java/util/ExcelToPdf/ConversionPdf.java
@@ -0,0 +1,32 @@
+package cn.mdk.common.utils.pdf;
+
+import java.io.*;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ConversionPdf {
+
+//    public static void main(String[] args) {
+//        if (args.length != 2) {
+//            System.err.println("Usage: java -jar excel-conversion-pdf.jar <input.xlsx> <output.pdf>");
+//            System.exit(0);
+//        }
+//
+//        String inputExcel = args[0];
+//        String outputPdf = args[1];
+//
+//        try {
+////            convertExcelToPdf(inputExcel, outputPdf);
+//            System.out.println("PDF化に成功しました。");
+//            System.exit(1);
+//        } catch (IOException e) {
+//            System.err.println("PDF化に失敗しました : " + e.getMessage());
+//            e.printStackTrace();
+//            System.exit(2);
+//        }
+//    }
+
+    public static void convertExcelToPdf(Workbook workbook, OutputStream outputStream) throws IOException {
+
+    }
+}
